f1ddaa1857a2fbe7bb631486a1e82aebdaeb01c0
Johnny Lin <hijohnnylin@gmail.com>
2019-05-12 02:50:32 -0700

    Update PM2 on deploy

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -3,6 +3,7 @@
 export NODE_ENV=production
 
 cd /home/node/helper
+pm2 updatePM2 # pick up new node version if we upgraded
 pm2 delete all
 pm2 start index.js -i max --merge-logs --log ../logs/app.log
 pm2 save
